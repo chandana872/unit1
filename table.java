549e6167ca472d954c6c2d8e14846a481975ec6e
chandana872 <67545850+chandana872@users.noreply.github.com>
2020-08-08 03:24:10 -0400

    Add files via upload

---
+++ table.java
@@ -0,0 +1,12 @@
+package Unit1;
+
+public class table {
+        public static void main(String[] args) {
+        	
+        	int m=2,n=12;
+        	
+        	for(int i=1;i<=n;i++) {
+        		System.out.println(m+"x"+i+"="+(m*i));
+        	}
+        }
+}
